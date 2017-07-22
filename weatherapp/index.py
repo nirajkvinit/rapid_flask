8a77a77a226e2b238f6ab46396925dd69706c4ec
Niraj Kumar <nirajkvinit@gmail.com>
2017-07-22 11:09:37 +0530

    Updated

--- weatherapp/index.py
+++ weatherapp/index.py
@@ -3,14 +3,19 @@ import os, json, time, urllib.request, urllib.error, urllib.parse
 
 app = Flask(__name__)
 
-def get_weather():
-    url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=London&cnt=10&mode=json&units=metric&appid=65b8831d1736fe05836815097ae4a457"
+def get_weather(city):
+    url = "http://api.openweathermap.org/data/2.5/forecast/daily?q={}&cnt=10&mode=json&units=metric&appid=65b8831d1736fe05836815097ae4a457".format(city)
     response = urllib.request.urlopen(url).read().decode('utf8');
     return response
 
 @app.route('/')
-def index():
-    data = json.loads(get_weather())
+@app.route("/<searchcity>")
+def index(searchcity = "London"):
+    data = json.loads(get_weather(searchcity))
+
+    city = data['city']['name']
+    country = data['city']['country']
+
     forecast_list = []
     for days in data.get("list"):
         day = time.strftime("%d %B", time.localtime(days.get('dt')))
@@ -18,7 +23,7 @@ def index():
         maxi = days.get("temp").get("max")
         description = days.get("weather")[0].get("description")
         forecast_list.append((day, mini, maxi, description))
-    return render_template("index.html", forecast_list=forecast_list)
+    return render_template("index.html", forecast_list=forecast_list, city=city, country=country)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 3000))
